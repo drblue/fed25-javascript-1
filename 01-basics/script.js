e69a68f48bf878684b76507b65a8c2fcc7e72d2a
Johan Nordström <johan@digitalvillage.se>
2025-09-29 11:54:20 +0200

    Naming conventions. Variables and constants.

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,5 +1,5 @@
 // Detta är en radkommentar
-console.log("Hello from script.js");  // Detta visar ett meddelande i konsollen
+// console.log("Hello from script.js");  // Detta visar ett meddelande i konsollen
 
 /*
 Detta är en block-kommentar
@@ -8,4 +8,59 @@ alert("I love JavaScript (I think)");
 console.log("I love my console");
 */
 
-alert("I NEED A <BR>");
+// alert("I NEED A <BR>");
+
+/*
+let msg = "Hello there";
+
+console.log(msg);
+
+console.log("Hello there again, it's me, a-mario");
+
+msg = "I am Luke";
+
+console.log(msg);
+
+let haxx0r;
+console.log(haxx0r);
+haxx0r = "I am haxx0r, very elite";
+console.log(haxx0r);
+*/
+
+/*
+// Naming Conventions
+
+// camelCase
+let myFirstName = "Johan";
+
+// snake_case
+let my_first_name = "Johan";
+
+// UpperCamelCase / PascalCase
+let MyFirstName = "Johan";
+
+// Kebab-Case / BBQ Case
+// let my-first-name = "Johan"; // Won't work in JavaScript!
+*/
+
+let firstName = "Johan";
+console.log(firstName);
+
+firstName = "Pelle";
+console.log(firstName);
+
+const lastName = "Nordström";
+console.log(lastName);
+
+// lastName = "Hackerson";  // NOPE!
+
+
+// String Concatenation
+console.log(firstName + " " + lastName);  // "Pelle Nordström"
+console.log("Pelle" + " " + "Nordström");  // "Pelle Nordström"
+
+let fullName = firstName + " " + lastName;  // "Pelle Nordström"
+console.log("Your name is: " + fullName);
+
+firstName = "Bengt";
+console.log("Your name is: " + fullName);  // "Pelle Nordström"
