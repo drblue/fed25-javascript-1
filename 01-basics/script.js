ae313a9a5acf010c6a9bbd79c23cffbe65d619ca
Johan Nordström <johan@digitalvillage.se>
2025-09-29 10:54:41 +0200

    Line comments and block comments in JavaScript.

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,5 +1,11 @@
-console.log("Hello from script.js");
+// Detta är en radkommentar
+console.log("Hello from script.js");  // Detta visar ett meddelande i konsollen
 
+/*
+Detta är en block-kommentar
 alert("I love JavaScript (I think)");
 
 console.log("I love my console");
+*/
+
+alert("I NEED A <BR>");
