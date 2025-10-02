820d1d1e981c2fd0bee7cbb0aea9b6c32fad4e65
Johan Nordström <johan@digitalvillage.se>
2025-10-02 13:23:38 +0200

    Array basics, part 2.

--- 06-arrays/script.js
+++ 06-arrays/script.js
@@ -2,6 +2,7 @@
  * Arrays
  */
 
+/*
 // 🥴
 let points1 = 1;
 let points2 = 42;
@@ -21,11 +22,13 @@ console.log("Students:", students);
 console.log("The second student's name is:", students[1]);
 
 console.log(`We have ${students.length} students in our class.`);
+*/
 
 /**
  * Non-destructive methods
  */
 
+/*
 // Join all items in an array to a string
 let names = students.join(", ");  // will always result in a string
 console.log("The students names are: " + names);
@@ -37,3 +40,37 @@ console.log("Index of Thanos:", students.indexOf("Thanos"));  // -1
 // Does "Black Widow" exist in the list?
 console.log("Does Black Widow exist in the list?", students.includes("Black Widow"));
 console.log("Does Super-Man exist in the list?", students.includes("Super-Man"));
+
+let friends = [
+	"Batman",
+	"Superman",
+	"Green Lantern",
+	"Joker",
+	"Harley Quinn",
+];
+
+// Concatenate two arrays together
+let mashup = students.concat(friends);
+console.log("MASHUP!!!!!!11", mashup);  // new array with 9 names
+console.log("students:", students);  // unchanged, still 4
+console.log("friends:", friends);  // unchanged, still 5
+*/
+
+/**
+ * Destructive methods
+ */
+
+let students = [ "Deadpool", "Black Widow", "Iron Man", "Spider-Man" ];
+let friends = [ "Batman", "Superman", "Green Lantern", "Joker", "Harley Quinn" ];
+
+console.log("Students before adding:", students);
+students.push("Wanda");
+console.log("Students after adding:", students);
+
+// Add "Thanos" to the list and then remove him because he wasn't nice
+students.push("Thanos");	// Add a string to the end of the array
+students.pop();				// Remove the last item in the array
+students.shift();			// Remove the first item in the array
+students.unshift("Hulk");	// Add a string to the start of the array
+
+console.log("The final students:", students);
