11040584509007dc40b282fc42624ffd5a645f46
Johan Nordström <johan@digitalvillage.se>
2025-10-06 13:32:39 +0200

    Strict mode.

---
+++ 12-strict/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * strict mode 👨🏻‍🏫
+ */
+
+// msg = "Hello from the otter side";  // nope! Uncaught ReferenceError: assignment to undeclared variable msg
+// console.log(msg);
+
+let title = "My first encounter with strict mode";
+// titel = "My second encounter with strict mode";
+
+console.log(title);
+
+// NaN = "LOL";
