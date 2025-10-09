ad15a3a4119eba1c4adc002688ee84815133b797
Johan Nordström <johan@digitalvillage.se>
2025-10-09 09:38:45 +0200

    Ternary operator and double negation/double bang.

--- 14-truthy-falsy-and-ternary/script.js
+++ 14-truthy-falsy-and-ternary/script.js
@@ -12,6 +12,7 @@
  * - -Infinity
  */
 
+/*
 let message = "abc";
 if (message) {
 	console.log("Message is truthy");
@@ -32,3 +33,37 @@ if (!username) {
 }
 
 console.log(`Welcome ${username}`);
+*/
+
+/*
+let username = prompt("Enter your name");
+if (!username) {
+	username = "Anonymous haxx0r";
+}
+console.log("Your name is:", username);
+*/
+
+/*
+let username = prompt("Enter your name");
+let msg;
+if (username) {
+	msg = `You have a name, it is ${username}, wow!`;
+} else {
+	msg = `Y U HAS NO NAME?!`;
+}
+console.log(msg);
+*/
+
+let username = prompt("Enter your name");
+let msg = username
+	? `You have a name, it is ${username}, wow!`
+	: `Y U HAS NO NAME?!`;
+console.log(msg);
+
+/**
+ * Double Negation / Double Bang 🤯
+ *
+ * For quick conversion to somethings boolean value
+ */
+console.log("the opposite of username is:", !username);
+console.log("the opposite of the opposite of username is:", !!username);
