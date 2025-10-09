e7f65f1a33198c1b42ad19c47589bada34861d71
Johan Nordström <johan@digitalvillage.se>
2025-10-09 13:55:09 +0200

    Workshop-solution: Password Checker with Functions - Step 1.

--- 16-workshop-password-checker-functions/script.js
+++ 16-workshop-password-checker-functions/script.js
@@ -31,19 +31,64 @@
  */
 
 const passwords = [
-	"password",								// 🚨
-	"p@*swd",								// 🔐
-	"pa$sword",								// 🔐
-	"p@ssw%rd",								// 🔐
-	"pa$$wd",								// 🔐
-	"secretpassword",						// 🚨
-	"secret-password",						// 🔐
-	"such-password-much-secure-very-long",	// 🔐
-	"$$$",									// 🚨
-];
+	"password",								// 🚨, index 0
+	"p@*swd",								// 🔐, index 1
+	"pa$sword",								// 🔐, index 2
+	"p@ssw%rd",								// 🔐, index 3
+	"pa$$wd",								// 🔐, index 4
+	"secretpassword",						// 🚨, index 5
+	"secret-password",						// 🔐, index 6
+	"such-password-much-secure-very-long",	// 🔐, index 7
+	"$$$",									// 🚨, index 8
+]; // length = 9
 
 const specialChars = [
 	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
 ];
 
 // 🤓 Skriv din kod här
+for (let i = 0; i < 9; i++) {
+	// get the password at index `i` from the array `passwords`
+	// and save it to the local variable `password`
+	const password = passwords[i];
+
+	console.log(`🕵🏻 Checking password '${password}'`);
+
+	// Räknare som indikerar hur många specialtecken som hittats
+	let specialCharCount = 0;
+
+	// För varje tecken i password, kolla om specialChars innehåller det tecknet
+	for (let i = 0; i < password.length; i++) {
+		const char = password[i];
+		// console.log(`At index ${i}, is char "${char}" a special char?`, specialChars.includes(char));
+
+		if (specialChars.includes(char)) {
+			specialCharCount++;
+		}
+
+		if (specialCharCount === 2) {  // Om vi hittat två specialtecken, sluta leta
+			break;  // Avbryt loopen
+		}
+	}
+	// console.log("Loop is done, found special chars:", specialCharCount);
+
+	// Har lösenordet minst 16 tecken?
+	if (password.length >= 6 && specialCharCount >= 2) {
+		console.log("- ✅ Such password, much secure, *VERY* hard to crack!");
+
+	} else if (password.length >= 8 && specialCharCount >= 1) {
+		console.log("- ✅ Such password, much secure, very hard to crack!");
+
+	} else if (password.length >= 12 && password.includes("-")) {
+		console.log("- ✅ Great! That's a pretty good password!");
+
+	} else if (password.length >= 16) {
+		console.log("- ✅ Great! That's a long password!");
+
+	} else {
+		console.log("- 🚨 Insecure password, my grandma can crack it!");
+
+	}
+
+}
+
