9ef595757145963f0e1d776e36bf69aa4b55aaef
Johan Nordström <johan@digitalvillage.se>
2025-10-13 13:31:16 +0200

    Guess the Number - Step 1.5.

--- 18-guess-the-number/script.js
+++ 18-guess-the-number/script.js
@@ -56,10 +56,15 @@ while (continueGame) {
 		alert("Y U GIVE UP?!");
 		continueGame = false;
 
+	} else if (guess > numberToGuess) {
+		// Guess was too high
+		console.log("Guess was too high 🤪");
+		alert("Guess was too high 🤪");
+
 	} else {
-		// Wrong guess
-		console.log("Guess was not correct 😬");
-		alert("This is not the number you are looking for");
+		// Guess was too low
+		console.log("Guess was too low 😔");
+		alert("Guess was too low 😔");
 
 	}
 }
