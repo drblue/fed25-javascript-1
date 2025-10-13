31a514319fd872a7203f5518f174bbe0b42871f6
Johan Nordström <johan@digitalvillage.se>
2025-10-13 11:09:50 +0200

    Object Literals with attributes/properties.

--- 19-objects/script.js
+++ 19-objects/script.js
@@ -5,7 +5,7 @@
 
 // Arrays aren't (that) suited for grouping related data together since they
 // are index-based and relies on the order of the values
-
+/*
 const fluffles = [
 	"Mr. Fluffles",		// 0 = name
 	3,					// 1 = age
@@ -25,6 +25,50 @@ const captainCat = [
 const showPetInfo = (pet) => {
 	// "NAME is a SPECIES of AGE year(s) old. His/her owner is OWNER and his/her favorite hobby is to HOBBY."
 	console.log(`${pet[0]} is a ${pet[4]} of ${pet[1]} year(s) old. His/her owner is ${pet[2]} and his/her favorite hobby is to ${pet[3]}.`);
+}
+
+showPetInfo(fluffles);
+showPetInfo(captainCat);
+*/
+
+// Objects are better suited for representing data that has an inherent
+// connection and is not dependent on the order of the keys.
+// Object use key/value-pairs.
+
+const fluffles = {
+	name: "Mr Fluffles",
+	age: 3,
+	owner: "Bengt",
+	hobbies: "Annoy people",
+	species: "Cat",
+}
+
+const captainCat = {
+	name: "Captain Cat",
+	species: "Cat",
+	age: 9,
+	owner: "Agda",
+	hobbies: "HIIT",
+	owner: "Tiffanny Persson",
+	"favorite-food": "Tuna",
+}
+
+console.log("fluffles:", fluffles);
+console.log("captainCat:", captainCat);
+
+// We access properties by using dot-notation
+console.log("Fluffle's name:", fluffles.name);
+console.log("Captain Cat's name:", captainCat.name);
+
+// We can also use square-bracket notation
+console.log("Fluffle's name using square-bracket notation:", fluffles["name"]);
+
+console.log("Captain Cats's favorite food:", captainCat["favorite-food"]);
+console.log("Fluffle's favorite food:", fluffles["favorite-food"]);
+
+const showPetInfo = (pet) => {
+	// "NAME is a SPECIES of AGE year(s) old. His/her owner is OWNER and his/her favorite hobby is to HOBBY."
+	console.log(`${pet.name} is a ${pet.species} of ${pet.age} year(s) old. His/her owner is ${pet.owner} and his/her favorite hobby is to ${pet.hobbies}.`);
 }
 
 showPetInfo(fluffles);
