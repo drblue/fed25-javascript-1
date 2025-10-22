f658fd5cf1df1a47e3164538eb8b799e28f380ed
Johan Nordström <johan@digitalvillage.se>
2025-10-22 11:22:23 +0200

    Sort an array numerically.

--- 25-higher-order-array-methods/01-sort.js
+++ 25-higher-order-array-methods/01-sort.js
@@ -11,6 +11,7 @@
  * - Shuffle an array
  */
 
+/*
 // names
 const names = ["Johan", "Pernilla", "Alicia", "Maja", "Elliot"];
 console.log("Names in total chaos:", names);
@@ -22,3 +23,34 @@ console.log("Names in alphabetical (proper) order 🤩:", names);
 // Reverse array
 names.reverse();
 console.log("Names in reverse alphabetical order 🤪:", names);
+*/
+
+// numbers
+const numbers = [ 47, 5, 28, 1337, 50, 21 ];
+console.log("Numbers in chaos:", numbers);
+
+// Sort numbers (won't sort numerically)
+// numbers.sort();
+// console.log("Numbers in proper(?) order:", numbers);
+
+// ACTUALLY sort numbers numerically
+numbers.sort( (a, b) => {
+	console.log(`comparing a: ${a} vs b: ${b}`);
+
+	// if `a` is less than `b` (i.e. `a` should be sorted BEFORE `b`)
+	if (a < b) {
+		console.log(`${a} is less than ${b} and should be placed BEFORE`);
+		return -1;
+	}
+
+	// if `a` is greater than `b` (i.e. `a` should be sorted AFTER `b`)
+	if (a > b) {
+		console.log(`${a} is greater than ${b} and should be placed AFTER`);
+		return 1;
+	}
+
+	console.log(`${a} is equal to ${b}`);
+	return 0;
+} );
+
+console.log("Numbers in proper! order:", numbers);
