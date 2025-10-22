88bd9be34d61eda7bb9e03d11079830ca54688af
Johan Nordström <johan@digitalvillage.se>
2025-10-22 14:10:54 +0200

    Filter based on an object's properties.

--- 25-higher-order-array-methods/02-filter.js
+++ 25-higher-order-array-methods/02-filter.js
@@ -11,8 +11,8 @@
  * - Shuffle an array
  */
 
-const numbers = [ 42, 5, 21, 50, 24, 18, 47 ];
-console.log("Original numbers:", numbers);
+// const numbers = [ 42, 5, 21, 50, 24, 18, 47 ];
+// console.log("Original numbers:", numbers);
 
 /*
 // Push all numbers >= 25 into a new array
@@ -52,6 +52,46 @@ const largeNumbers = numbers.filter((num) => {
 */
 
 // Filter out all numbers >= 25 (OMG ITS SO SHORT!!!!!!!!!!!!! 🤯🤯🤯)
-const largeNumbers = numbers.filter((num) => num >= 25);
+// const largeNumbers = numbers.filter((num) => num >= 25);
 
-console.log("Large numbers:", largeNumbers);
+// console.log("Large numbers:", largeNumbers);
+
+/**
+ * We can also filter on an object's properties
+ */
+
+const students = [
+	{
+		name: "Johan",
+		points: 1337,
+	},
+	{
+		name: "Pernilla",
+		points: -3,
+	},
+	{
+		name: "Alicia",
+		points: 88,
+	},
+	{
+		name: "Maja",
+		points: 35,
+	},
+	{
+		name: "Elliot",
+		points: 42,
+	},
+];
+
+// Get all students which are godkända (>= 40)
+const passed = students.filter((student) => {
+	return student.points >= 40;
+	/*
+	if (student.points >= 40) {
+		return true;
+	} else {
+		return false;
+	}
+	*/
+});
+console.log("Students who passed the exam:", passed);
