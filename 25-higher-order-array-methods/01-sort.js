07c8b7b8993b6c41ab44ff27ec3e7d65a4f1488c
Johan Nordström <johan@digitalvillage.se>
2025-10-22 11:04:57 +0200

    Sort an array alphabetically. Also - reverse the order of an array.

--- 25-higher-order-array-methods/01-sort.js
+++ 25-higher-order-array-methods/01-sort.js
@@ -14,3 +14,11 @@
 // names
 const names = ["Johan", "Pernilla", "Alicia", "Maja", "Elliot"];
 console.log("Names in total chaos:", names);
+
+// Sort array alphabetically
+names.sort();
+console.log("Names in alphabetical (proper) order 🤩:", names);
+
+// Reverse array
+names.reverse();
+console.log("Names in reverse alphabetical order 🤪:", names);
