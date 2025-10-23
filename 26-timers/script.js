05aae85cc4b8c7ab49fbe5ab38c2a67fdb472fac
Johan Nordström <johan@digitalvillage.se>
2025-10-23 11:21:20 +0200

    Scare the user with a 👻 and `setTimeout()`.

--- 26-timers/script.js
+++ 26-timers/script.js
@@ -5,3 +5,20 @@
 
 const btnScareEl = document.querySelector('#btnScareMe');
 const ghostEl = document.querySelector('#ghost');
+
+btnScareEl.addEventListener("click", () => {
+	// hide button
+	btnScareEl.classList.add("hide");
+
+	// start timer
+	console.log("Starting timer for scaring user 😈");
+	setTimeout(() => {
+		// this will be executed after (approximately) 5 seconds
+		console.log("Scaring user... 😱");
+
+		// show ghost
+		ghostEl.classList.remove("hide");
+	}, 5000);
+
+	console.log("Timer has started, prepare to be scaaaaarreeeed 🦇");
+});
