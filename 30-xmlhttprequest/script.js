f28b1a008babe3bd2fe4090524ddb80ef7e4d64d
Johan Nordström <johan@digitalvillage.se>
2025-11-03 13:56:20 +0100

    Wrapped request in a function called `getUsers()`. Added callback so we can choose what to do with the response.

--- 30-xmlhttprequest/script.js
+++ 30-xmlhttprequest/script.js
@@ -3,47 +3,79 @@
  *
  */
 
-// Create a new XMLHttpRequest
-const request = new XMLHttpRequest();
-console.log("Request readyState after new:", request.readyState);
+const getUsers = (callback) => {
+	// Create a new XMLHttpRequest
+	const request = new XMLHttpRequest();
+	console.log("Request readyState after new:", request.readyState);
 
-// Set request to GET data from "https://jsonplaceholder.typicode.com/users"
-request.open("GET", "https://jsonplaceholder.typicode.com/users");
-console.log("Request readyState after open:", request.readyState);
+	// Set request to GET data from "https://jsonplaceholder.typicode.com/users"
+	request.open("GET", "https://jsonplaceholder.typicode.com/users");
+	console.log("Request readyState after open:", request.readyState);
 
-// Attach an event-listener to the request
-request.addEventListener("readystatechange", () => {
-	// Log request status
-	console.log("readystatechange - readyState:", request.readyState);
-	// console.log("readystatechange - responseText:", request.responseText);
+	// Attach an event-listener to the request
+	request.addEventListener("readystatechange", () => {
+		// Log request status
+		console.log("readystatechange - readyState:", request.readyState);
+		// console.log("readystatechange - responseText:", request.responseText);
 
-	// Is request done?
-	if (request.readyState === 4) {
-		// Yay request is finished!
+		// Is request done?
+		if (request.readyState === 4) {
+			// Yay request is finished!
 
-		// Was the request successful?
-		if (request.status === 200) {
-			// 200 OK 👍🏻
-			// console.log("🆗 Request done, yay! Response was:", request.responseText);
+			// Was the request successful?
+			if (request.status === 200) {
+				// 200 OK 👍🏻
+				// console.log("🆗 Request done, yay! Response was:", request.responseText);
 
-			// Take the JSON-STRING and PARSE it into a JavaScript value
-			const data = JSON.parse(request.responseText);
-			console.log("🆗 Response OK & done. Got me sum yummy data:", data);
+				// Take the JSON-STRING and PARSE it into a JavaScript value
+				const data = JSON.parse(request.responseText);
+				console.log("🆗 Response OK & done. Got me sum yummy data:", data);
 
-			// Transform the data-array into listitems and output to DOM
-			document.querySelector("#users").innerHTML = data
-				.map((user) => `<li>${user.name}</li>`)
-				.join("");
+				callback(false, data);
 
-		} else {
-			console.error("🚨 DANGER DANGER WILL ROBINSON!!!!!!!!");
+			} else {
+				console.error("🚨 DANGER DANGER WILL ROBINSON!!!!!!!!");
+				callback("Error getting data! Status code was: " + request.status);
+			}
 		}
+	});
+
+	// Send the request
+	request.send();
+	console.log("Request readyState after send:", request.readyState);
+
+	// Done (?)
+	console.log("Request sent!");
+}
+
+getUsers( (err, data) => {
+	console.log("📞 Hello from callback");
+	console.log("err:", err);
+	console.log("data:", data);
+
+	if (err) {
+		// Something went wrong 😢
+		alert(err);
+		return;
 	}
-});
 
-// Send the request
-request.send();
-console.log("Request readyState after send:", request.readyState);
+	// Transform the data-array into listitems and output to DOM
+	document.querySelector("#users").innerHTML = data
+		.map((user) => `<li>${user.name}</li>`)
+		.join("");
+} );
 
-// Done (?)
-console.log("Request sent!");
+getUsers( (err, data) => {
+	console.log("📞 Hello from callback");
+	console.log("err:", err);
+	console.log("data:", data);
+
+	if (err) {
+		// Something went wrong 😢
+		alert(err);
+		return;
+	}
+
+	// Transform the data-array into listitems and output to DOM
+	console.log(data.map(user => user.name).join(", "));
+} );
