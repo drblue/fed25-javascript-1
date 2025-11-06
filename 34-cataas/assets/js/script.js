6b1b1501dad3aa7860ba9e601b2a3b3ef7bc8659
Johan Nordström <johan@digitalvillage.se>
2025-11-06 13:30:32 +0100

    Micro-workshop: Get random meowster image on button click.

--- 34-cataas/assets/js/script.js
+++ 34-cataas/assets/js/script.js
@@ -2,6 +2,10 @@
  * Cataas 🐈🐶
  *
  * <https://cataas.com/>
+ *
+ * When button is clicked, make a fetch-request to
+ * `https://cataas.com/cat?json=true`, convert it from JSON
+ * and then set the image src to the `url` from the response.
  */
 
 console.log("🐈");
