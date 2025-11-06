45c54c0944f295d74d941ec7df7265504a9c77c6
Johan Nordström <johan@digitalvillage.se>
2025-11-06 14:14:55 +0100

    🐈 Cats as a Service! 😻

--- 34-cataas/assets/js/script.js
+++ 34-cataas/assets/js/script.js
@@ -9,3 +9,30 @@
  */
 
 console.log("🐈");
+
+// Get reference to image element
+const catImageEl = document.querySelector("#cat-image");
+
+// Attach click-eventlistener to the button
+document.querySelector("#cat-button").addEventListener("click", () => {
+	console.log("Getting kitteh...");
+
+	// Get kitteh from `https://cataas.com/cat?json=true`
+	fetch("https://cataas.com/cat?json=true")
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error("Could not find kitteh");
+			}
+
+			return res.json();
+		})
+		.then((cat) => {
+			console.log("🐱:", cat);
+
+			// Change image to the kitteh from the API response
+			catImageEl.setAttribute("src", cat.url);
+		})
+		.catch((err) => {
+			console.log("🚨 Failed to fetch kitteh because:", err);
+		});
+});
