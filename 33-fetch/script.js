ce333d90ce56bc0447b259ee3274c8893391ea09
Johan Nordström <johan@digitalvillage.se>
2025-11-06 12:02:19 +0100

    Get dogs using `fetch()` 🐶🎾.

--- 33-fetch/script.js
+++ 33-fetch/script.js
@@ -2,3 +2,23 @@
  * Fetch 🐶
  *
  */
+
+console.log("Starting fetch 🎾🐶...");
+
+fetch("data/snakes.json")
+	.then((response) => {
+		console.log("Got response:", response);
+
+		if (!response.ok) {
+			// FAIL
+			throw new Error("Request was not OK!");
+		}
+
+		return response.json();
+	})
+	.then((data) => {
+		console.log("Got data:", data);
+	})
+	.catch((err) => {
+		console.log("🚨 Something went wrong:", err);
+	});
