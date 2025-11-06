1171ef8891403dc5b4d67475bd93342999fd017a
Johan Nordström <johan@digitalvillage.se>
2025-11-06 13:08:07 +0100

    Fetch `dogs.json` and `cats.json` in parallel.

--- 33-fetch/script.js
+++ 33-fetch/script.js
@@ -1,23 +1,42 @@
 /**
  * Fetch 🐶
  *
+ * Get `dogs.json` and `cats.json` in parallel
  */
 
 console.log("Starting fetch 🎾🐶...");
 
-fetch("data/snakes.json")
+// Get doggos
+fetch("data/dogs.json")
 	.then((response) => {
-		console.log("Got response:", response);
-
 		if (!response.ok) {
-			// FAIL
 			throw new Error("Request was not OK!");
 		}
 
 		return response.json();
 	})
 	.then((data) => {
-		console.log("Got data:", data);
+		document.querySelector("#dogs").innerHTML = data
+			.map(dog => `<li>${dog.name}</li>`)
+			.join("");
+	})
+	.catch((err) => {
+		console.log("🚨 Something went wrong:", err);
+	});
+
+// Get cattos
+fetch("data/cats.json")
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error("Request was not OK!");
+		}
+
+		return response.json();
+	})
+	.then((data) => {
+		document.querySelector("#cats").innerHTML = data
+			.map(cat => `<li>${cat.name}</li>`)
+			.join("");
 	})
 	.catch((err) => {
 		console.log("🚨 Something went wrong:", err);
