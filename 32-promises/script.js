1b2c28db697bf1925f7938bef96811160f3dd708
Johan Nordström <johan@digitalvillage.se>
2025-11-06 11:28:41 +0100

    Using promises to get data in parallel.

--- 32-promises/script.js
+++ 32-promises/script.js
@@ -3,6 +3,7 @@
  *
  */
 
+/*
 // Create a promise that will be either resolved or rejected in the future
 const promiseOfFutureData = () => {
 	const promiseOfData = new Promise( (resolve, reject) => {
@@ -24,41 +25,73 @@ promiseOfFutureData()
 	.catch((err) => {
 		console.log("Promise was broken 😢:", err);
 	});
-
+*/
 
 // Get data from a URL
-const getJSON = (url, callback) => {
-	// Create a new XMLHttpRequest
-	const request = new XMLHttpRequest();
+const getJSON = (url) => {
+	// Return a new Promise
+	return new Promise((resolve, reject) => {
+		// The actual promise-code
 
-	// Set request to GET data from the URL
-	request.open("GET", url);
+		// Create a new XMLHttpRequest
+		const request = new XMLHttpRequest();
 
-	// Attach an event-listener to the request
-	request.addEventListener("readystatechange", () => {
-		// Is request done?
-		if (request.readyState === 4) {
-			// Yay request is finished!
+		// Set request to GET data from the URL
+		request.open("GET", url);
 
-			// Was the request successful?
-			if (request.status === 200) {
-				// 200 OK 👍🏻
+		// Attach an event-listener to the request
+		request.addEventListener("readystatechange", () => {
+			// Is request done?
+			if (request.readyState === 4) {
+				// Yay request is finished!
 
-				// Take the JSON-STRING and PARSE it into a JavaScript value
-				const data = JSON.parse(request.responseText);
+				// Was the request successful?
+				if (request.status === 200) {
+					// 200 OK 👍🏻
 
-				callback(false, data);
+					// Take the JSON-STRING and PARSE it into a JavaScript value
+					const data = JSON.parse(request.responseText);
 
-			} else {
-				// Something went wrong ❌
-				console.error("🚨 DANGER DANGER WILL ROBINSON!!!!!!!!");
-				callback("Error getting data! Status code was: " + request.status);
+					// callback(false, data);
+					resolve(data);
+
+				} else {
+					// Something went wrong ❌
+					console.error("🚨 DANGER DANGER WILL ROBINSON!!!!!!!!");
+					// callback("Error getting data! Status code was: " + request.status);
+					reject("Error getting data! Status code was: " + request.status)
+				}
 			}
-		}
+		});
+
+		// Send the request 📮
+		request.send();
+
+		console.log("Request sent to:", url);
+	});
+}
+
+/*
+const promiseOfDogs = getJSON("data/dogs.json");
+console.log("promiseOfDogs:", promiseOfDogs);
+
+const promiseOfCats = getJSON("data/cats.json");
+console.log("promiseOfCats:", promiseOfCats);
+*/
+
+// getJSON("data/dogs.json", (err, data) => {});
+getJSON("data/dogs.json")
+	.then((dogs) => {
+		console.log("Yay dogs:", dogs);
+	})
+	.catch((err) => {
+		console.error("No dogs 😢:", err);
 	});
 
-	// Send the request 📮
-	request.send();
-
-	console.log("Request sent to:", url);
-}
+getJSON("data/cats.json")
+	.then((cats) => {
+		console.log("Yay cats:", cats);
+	})
+	.catch((err) => {
+		console.error("No cats 😢:", err);
+	});
