322e5295fdb4d704dc6dda57cab1ea51db3b45d9
Johan Nordström <johan@digitalvillage.se>
2025-11-12 00:41:27 +0100

    Weather app: Boilerplate.

---
+++ 37-weather-app/assets/scripts/app.js
@@ -0,0 +1,8 @@
+/**
+ * 🌧️.
+ *
+ */
+
+const alertEl = document.querySelector("#alert");
+const forecastEl = document.querySelector("#forecast");
+const spinnerEl = document.querySelector("#spinner");
