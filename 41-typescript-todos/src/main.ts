6158c817cabbbcf51aa9280ceb11033649901c92
Johan Nordström <johan@digitalvillage.se>
2025-11-19 10:32:26 +0100

    Render the initial list of todos to DOM.

--- 41-typescript-todos/src/main.ts
+++ 41-typescript-todos/src/main.ts
@@ -1,2 +1,33 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
+
+const todolistEl = document.querySelector<HTMLUListElement>("#todolist")!;
+
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
+let todos: Todo[] = [
+	{ id: 1, title: "Wake up", completed: true },
+	{ id: 2, title: "Drink coffee", completed: true },
+	{ id: 3, title: "Code", completed: false },
+	{ id: 4, title: "Sleep", completed: false },
+];
+
+/**
+ * Render todos to DOM
+ */
+const renderTodos = () => {
+	todolistEl.innerHTML = todos
+		.map((todo) => `
+			<li class="list-group-item todo ${todo.completed ? "completed" : ""}" data-todo-id="${todo.id}">
+				<span class="todo-title">${todo.title}</span>
+				<button class="btn btn-danger btn-sm" aria-label="Delete">💣</button>
+			</li>
+		`)
+		.join("");
+}
+
+renderTodos();
