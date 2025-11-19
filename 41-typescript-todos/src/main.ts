74ae5e76fbe5b2a60649894a2b1900c7be31ea68
Johan Nordström <johan@digitalvillage.se>
2025-11-19 10:50:43 +0100

    Toggle todo.

--- 41-typescript-todos/src/main.ts
+++ 41-typescript-todos/src/main.ts
@@ -30,4 +30,39 @@ const renderTodos = () => {
 		.join("");
 }
 
+/**
+ * Listen for clicks on the todolist
+ */
+todolistEl.addEventListener("click", (e) => {
+	console.log("You clicked, wow!", e.target);
+
+	// Promise TypeScript that e.target actually is a HTMLElement
+	const targetEl = e.target as HTMLElement;
+ //     ^?
+
+	// Was the click on a todo title (span)?
+	if (targetEl.tagName === "SPAN") {
+		// We should toggle the todo
+
+		// Get ID of todo from parent listitem element
+		const clickedTodoId = Number(targetEl.parentElement?.dataset.todoId);
+
+		// Find the todo with the ID
+		const clickedTodo = todos.find((todo) => {
+			return todo.id === clickedTodoId;
+		});
+
+		// If we didn't (somehow) find the todo (very unlikely, but still), bail 🏃🏻‍♂️💨
+		if (!clickedTodo) {
+			return;
+		}
+
+		// Ok, we're sure the todo exists, so let's invert the `completed` value
+		clickedTodo.completed = !clickedTodo.completed;
+
+		// Re-render the list
+		renderTodos();
+	}
+});
+
 renderTodos();
