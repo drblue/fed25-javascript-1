61b1996bc46316036656385814aa7003fe582f86
Johan Nordström <johan@digitalvillage.se>
2025-11-24 10:02:41 +0100

    Added environment variables (and typing of them) along with a `.env.example` file. N.B.! DON'T FORGET TO ADD `.env` to your `.gitignore` BEFORE COMMITING!

--- 43-weather-app-ts/src/main.ts
+++ 43-weather-app-ts/src/main.ts
@@ -1,1 +1,4 @@
 import "./assets/scss/app.scss";
+
+console.log("Environment variables:", import.meta.env);
+console.log("API key from env is:", import.meta.env.VITE_API_KEY);
---
+++ 43-weather-app-ts/src/vite-env.d.ts
@@ -0,0 +1,15 @@
+interface ViteTypeOptions {
+	// By adding this line, you can make the type of ImportMetaEnv strict
+	// to disallow unknown keys.
+	// strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+	readonly VITE_API_KEY?: string;
+	readonly VITE_API_BASE_URL?: string;
+	readonly VITE_API_FAKE_DELAY?: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
